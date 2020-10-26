bdc047baf52e4b0945c9dda0c3c4fe9b4693a8b5
Zetrax <34830529+theZetrax@users.noreply.github.com>
2020-10-26 08:46:43 +0300

    Final Commit, Everything is working fine.

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    <a href="{{ route('posts.index') }}" class="btn btn-info text-white mb-3">Go Back</a>
     <h1>{{ $post->title }}</h1>
     <small>{{ $post->created_at }}</small>
     <p>{{ $post->body }}</p>
     <div>
-        {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'POST']) !!}
-            {!! Form::hidden('_method', 'DELETE') !!}
-            <a href="{{ route('posts.edit', [$post->id]) }}" class="btn btn-primary text-white">Edit</a>
-            {!! Form::submit('Delete', ['class' => 'btn btn-danger text-white']) !!}
-            <a href="{{ route('posts.index') }}" class="btn btn-primary-outline text-dark">Cancel</a>
-        {!! Form::close() !!}
+        @auth
+            {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'POST']) !!}
+                {!! Form::hidden('_method', 'DELETE') !!}
+                <a href="{{ route('posts.edit', [$post->id]) }}" class="btn btn-success text-white">Edit</a>
+                {!! Form::submit('Delete', ['class' => 'btn btn-danger text-white']) !!}
+            {!! Form::close() !!}
+        @endauth
     </div>
 @endsection
